1b009ef077f53ff83123319592ead2797cb816d8
Elizabeth Restrepo <elizaretrepo5@gmail.com>
2024-06-08 18:53:07 -0500

    V12

--- pages/1_Proyecto_integrador.py
+++ pages/1_Proyecto_integrador.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import os
 
 st.title("Proyecto Integrador - Ventas Restaurante")
 
 # Especificar la ruta del archivo CSV
-file_path = 'static\Restaurante.csv'
+file_path = 'static/Restaurante.csv'
 
 # Intentar leer el archivo CSV con diferentes encodings
 try:
@@ -21,23 +22,28 @@ if 'Fecha' not in df.columns:
     st.error("La columna 'Fecha' no se encontró en el archivo de datos. Asegúrate de que el archivo CSV tenga la columna 'Fecha'.")
     st.stop()
 
-# Convertir la columna 'Fecha' a datetime
+# Convertir la columna 'Fecha' a datetime y separar la hora si está en la misma columna
 df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d/%m/%Y', errors='coerce')
 
-# Filtrar las filas con fechas no convertibles
-df = df.dropna(subset=['Fecha'])
+# Si la hora está en una columna separada, convertirla a formato datetime y combinarla con la fecha
+if 'Hora' in df.columns:
+    df['Hora'] = pd.to_datetime(df['Hora'], format='%I:%M:%S %p', errors='coerce').dt.time
+    df['Fecha_Hora'] = df.apply(lambda row: pd.datetime.combine(row['Fecha'], row['Hora']), axis=1)
+else:
+    df['Fecha_Hora'] = df['Fecha']
 
-# Extraer mes y hora de la columna 'Fecha'
-df['Mes'] = df['Fecha'].dt.to_period('M')
-df['Dia'] = df['Fecha'].dt.date
-df['Hora'] = df['Fecha'].dt.hour
+# Filtrar las filas con fechas no convertibles
+df = df.dropna(subset=['Fecha_Hora'])
+
+# Extraer mes, día y hora de la columna 'Fecha_Hora'
+df['Mes'] = df['Fecha_Hora'].dt.to_period('M')
+df['Dia'] = df['Fecha_Hora'].dt.date
+df['Hora'] = df['Fecha_Hora'].dt.hour
 
 # Obtener las opciones únicas de cada filtro
 productosU = sorted(df['Producto'].unique())
 mesesU = sorted(df['Mes'].unique().astype(str))
 diasU = sorted(df['Dia'].unique())
-horasU = sorted(df['Hora'].unique())
-
 # Configurar las columnas y selectores
 col1, col2 = st.columns(2)
 
@@ -56,8 +62,7 @@ with col3:
     optionDia = st.selectbox('Día', (diasU))
 
 with col4:
-    horasU = list(range(12))
-    optionHora = st.slider('Rango de Hora', min_value=12, max_value=23, value=(12, 23))    
+    optionHora = st.slider('Rango de Hora', min_value=0, max_value=23, value=(0, 23))
 
 # Filtrar los datos según las opciones seleccionadas
 filtered_data = df
@@ -70,12 +75,12 @@ if optionProducto != "Todos":
 if optionDia != "Todos":
     filtered_data = filtered_data[filtered_data['Dia'] == optionDia]
 
-if optionHora != "Todas":
-    filtered_data = filtered_data[filtered_data['Hora'] == int(optionHora)]
+start_hour, end_hour = optionHora
+filtered_data = filtered_data[(filtered_data['Hora'] >= start_hour) & (filtered_data['Hora'] <= end_hour)]
 
-# Crear un gráfico de barras para el total de ventas por producto
+# Crear un gráfico de barras horizontales para el total de ventas por producto
 ventas_por_producto = filtered_data.groupby('Producto')['Total'].sum().reset_index()
-fig_bar = px.bar(ventas_por_producto, x='Producto', y='Total', title='Total de Ventas por Producto')
+fig_bar_horizontal = px.bar(ventas_por_producto, x='Total', y='Producto', orientation='h', title='Total de Ventas por Producto (Horizontal)')
 
 # Crear un gráfico de líneas para la evolución de ventas en el tiempo
 ventas_por_fecha = filtered_data.groupby('Fecha')['Total'].sum().reset_index()
@@ -93,7 +98,7 @@ fig_bar_dia = px.bar(ventas_por_dia, x='Dia', y='Total', title='Ventas por Día'
 fig_pie = px.pie(ventas_por_producto, values='Total', names='Producto', title='Proporción de Ventas por Producto')
 
 # Mostrar los gráficos
-st.plotly_chart(fig_bar, use_container_width=True)
+st.plotly_chart(fig_bar_horizontal, use_container_width=True)
 st.plotly_chart(fig_line, use_container_width=True)
 st.plotly_chart(fig_bar_hora, use_container_width=True)
 st.plotly_chart(fig_bar_dia, use_container_width=True)
